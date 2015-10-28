360dff930d748282a37f388fb7678906c3f5c1f3
Petar Petrov <ppetrov@bluebuffstudio.com>
2015-10-28 10:53:56 +0200

    example for selection plugin

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,23 @@
-console.log('This would be the main JS file.');
+document.addEventListener('DOMContentLoaded', function() {
+    OpenSeadragon({
+        id: 'contentDiv',
+        prefixUrl: '/images/buttons/',
+        tileSources: {
+            Image: {
+                xmlns: 'http://schemas.microsoft.com/deepzoom/2008',
+                Url: 'http://openseadragon.github.io/example-images/duomo/duomo_files/',
+                Format: 'jpg',
+                Overlap: '2',
+                TileSize: '256',
+                Size: {
+                    Width:  '13920',
+                    Height: '10200'
+                }
+            }
+        }
+    }).selection({
+        onSelection: function(rect) {
+            alert(rect + ' Center point: ' + rect.getCenter() + ' Degree rotation: ' + rect.getDegreeRotation());
+        }
+    });
+});
