96b5b2f2ac534cde24870427efacc9f2f5f23850
Petar Petrov <ppetrov@bluebuffstudio.com>
2015-12-07 12:54:46 +0200

    added zoom levels plugin

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,11 +1,12 @@
-var viewer, selection, rgb;
+var viewer, selection, rgb, levels;
 document.addEventListener('DOMContentLoaded', function() {
     viewer = OpenSeadragon({
         id: 'contentDiv',
         prefixUrl: 'images/buttons/',
         crossOriginPolicy: 'Anonymous',
-        defaultZoomLevel: 0,
+        defaultZoomLevel: 1.1,
         tileSources: 'http://openseadragon.github.io/example-images/highsmith/highsmith.dzi',
+        minZoomImageRatio: 0.1, // of viewer size
     });
     selection =viewer.selection({
         onSelection: function(rect) {
@@ -22,4 +23,16 @@ document.addEventListener('DOMContentLoaded', function() {
             document.getElementById('img').checked = !!color.image;
         }
     });
+    levels = viewer.zoomLevels({
+        levels: [0.04, 0.05, 0.07, 0.1, 0.15, 0.2, 0.3, 0.4, 0.5, 0.65, 0.8, 1]
+        // levels: [0.1, 1]
+    });
+    var zoomView = $('#zoom');
+    viewer.addHandler('zoom', function(e) {
+        var percentage = viewer.world.getItemAt(0).viewportToImageZoom(e.zoom);
+        zoomView.val(percentage.toFixed(2));
+    });
+
+    var active = $('#tabs > div').index($(location.hash));
+    $('#tabs').tabs({active: active === -1 ? null : active});
 });
