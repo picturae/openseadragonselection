f035aa8a6cc4d7f96d6db639c5a05c96f341b249
M. Nijdam <m.nijdam@picturae.nl>
2016-07-12 11:11:19 +0200

    local dzi to work around cors issue wit rgb plugin

--- javascripts/main.js
+++ javascripts/main.js
@@ -6,7 +6,8 @@ document.addEventListener('DOMContentLoaded', function() {
         crossOriginPolicy: 'Anonymous',
         defaultZoomLevel: 1.1,
         //zoomLevels: [0.04, 0.05, 0.07, 0.1, 0.15, 0.2, 0.3, 0.4, 0.5, 0.65, 0.8, 1], //only for Picturae fork of openseadragon
-        tileSources: 'https://openseadragon.github.io/example-images/highsmith/highsmith.dzi',
+        //tileSources: 'https://openseadragon.github.io/example-images/highsmith/highsmith.dzi', //issues with rgb plugin and CORS
+        tileSources: 'images/example/Picturae_Quality.dzi',
         minZoomImageRatio: 0.1, // of viewer size
         //navigationControlAnchor: OpenSeadragon.ControlAnchor.BOTTOM_LEFT,
         immediateRender: true
@@ -19,7 +20,7 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     });
 
-    //regb plugin
+    //rgb plugin
     rgb = viewer.rgb({ //does not work in fullscreen since elements do not exist in fullscreen
         onCanvasHover: function(color) {
             document.getElementById('r').value = color.r;
